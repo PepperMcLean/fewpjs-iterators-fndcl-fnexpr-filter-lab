cecf9e62d64b328192d638fc93c86b97493fe342
PepperMcLean <jackmcl206@gmail.com>
2022-01-16 23:28:48 -0600

    commit tree 144a9b114f1569fdc652a0b6aeaed515bf0c7555

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+function findMatching(drivers, driver) {
+  return drivers.filter( maybeDriver => maybeDriver.toLowerCase() === driver.toLowerCase()
+  )
+}
+
+function fuzzyMatch(drivers, firstLetter) {
+  return drivers.filter( maybeDriver => maybeDriver.toLowerCase().indexOf(firstLetter.toLowerCase()) === 0)
+}
+
+function matchName(drivers, driver) {
+  return drivers.filter( maybeDriver => maybeDriver.name === driver)
+}
